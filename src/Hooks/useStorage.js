3d5a04d5380c979c371b7a14942a5186d6885a40
oleg <vinodevmail@gmail.com>
2020-11-22 14:32:37 +0200

    temporery fix when config file does not exist

--- src/Hooks/useStorage.js
+++ src/Hooks/useStorage.js
@@ -1,7 +1,13 @@
 import {useState, useEffect} from 'react';
+
+//Commented-out to avoid run time errors when no config file, permanent solution needed.
+//#####################################################################################
+
+/*
 import {projectFirestore, timestamp} from '../firebase/config';
 
 const collectionRef = projectFirestore.collection("users");
 const createdAt = timestamp();
 
 //collectionRef.add({email: "test@mail,com", createdAt});
+*/
