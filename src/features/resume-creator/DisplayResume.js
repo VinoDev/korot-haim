7fee372c2d2c98229166680e8593ee8986aec127
Tal Gold <talpogorelis@gmail.com>
2020-12-28 03:36:35 +0200

    first test version for react-pdf library instead of jsPDF

--- src/features/resume-creator/DisplayResume.js
+++ src/features/resume-creator/DisplayResume.js
@@ -1,51 +1,61 @@
 import React, { useState } from "react";
-import ResumeOne from "./templates/ResumeOne";
-import jsPDF from "jspdf";
-import html2canvas from "html2canvas";
-import { Button } from '@material-ui/core';
-import GetAppRoundedIcon from '@material-ui/icons/GetAppRounded';
-import "./displayResume.css"
+import { Button } from "@material-ui/core";
+import GetAppRoundedIcon from "@material-ui/icons/GetAppRounded";
+import "./displayResume.css";
+
+import { PDFViewer, PDFDownloadLink } from "@react-pdf/renderer";
+import { ResumeDoc } from "./templates/ResumeDoc";
 
 function DisplayResume(props) {
-  // const [resume, setResume] = useState();
-
-  // const exportPDF = () => {
-  //   resume.save();
-  // };
-  const doc = new jsPDF();
-  const captureResume = () => {
-    html2canvas(document.querySelector("#resume-capture")).then((canvas) => {
-      //document.body.appendChild(canvas); // if you want see your screenshot in body.
-      const imgData = canvas.toDataURL("image/png");
-
-      //document.write('<img src="' + imgData + '"/>');
-      const pdf = new jsPDF();
-      pdf.addImage(imgData, "PNG", 0, 0);
-      pdf.save("download.pdf");
-    });
-  };
+  const [link, setLink] = useState();
 
   return (
     <div>
-      <div id="resume-capture">
-        <ResumeOne />
-      </div>
-      <div style={{display:"flex", justifyContent:"center"}}>
+      <PDFViewer
+        style={{
+          width: "100%",
+          height: "500px",
+          display: "flex",
+          justifyContent: "center",
+          borderRadius: "50px",
+        }}
+        id="resume-capture"
+        id="pdf-viewer"
+      >
+        <ResumeDoc />
+      </PDFViewer>
+      <div style={{ display: "flex", justifyContent: "center" }}>
         <Button
           variant="contained"
           size="large"
           color="secondary"
           style={{
-            fontSize:"22px",
-            marginTop:"14px",
-            alignContent:"center",
-            fontWeight:"bold",
-            width:"250px"
+            fontSize: "22px",
+            marginTop: "14px",
+            alignContent: "center",
+            fontWeight: "bold",
+            width: "250px",
+            textDecoration: "none",
+            color: "white",
           }}
-          onClick={captureResume}
+          href={link}
+          target="_blank"
         >
-          הורד
-        <GetAppRoundedIcon style={{marginTop:"4px"}}/>
+          <PDFDownloadLink
+            id="pdf-download-link"
+            document={<ResumeDoc />}
+            fileName="cv.pdf"
+          >
+            {({ blob, url, loading, error }) => {
+              if (loading) {
+                return <div>Loading document...</div>;
+              } else {
+                setLink(url);
+                return <div>הורד</div>;
+              }
+            }}
+          </PDFDownloadLink>
+          <GetAppRoundedIcon style={{ marginTop: "4px" }} />
         </Button>
       </div>
     </div>
